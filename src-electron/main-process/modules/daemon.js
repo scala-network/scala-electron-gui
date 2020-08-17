5b78769467dccfae5bf6dc1dbe6b871ec2d16139
Teredic <40125338+Teredic@users.noreply.github.com>
2020-08-17 18:38:30 +0200

    Update Blocktime for height conversion

--- src-electron/main-process/modules/daemon.js
+++ src-electron/main-process/modules/daemon.js
@@ -20,9 +20,9 @@ export class Daemon {
 
         // Settings for timestamp to height conversion
         // These are initial values used to calculate the height
-        this.PIVOT_BLOCK_HEIGHT = 119681
-        this.PIVOT_BLOCK_TIMESTAMP = 1523976903
-        this.PIVOT_BLOCK_TIME = 300
+        this.PIVOT_BLOCK_HEIGHT = 11730
+        this.PIVOT_BLOCK_TIMESTAMP = 1597688400
+        this.PIVOT_BLOCK_TIME = 120
     }
 
     checkVersion () {
